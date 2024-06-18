b2c6fb871d79daf81de5f677956015677387a54b
R Mohan Krishna <141043010+rmohan7603@users.noreply.github.com>
2024-06-18 12:51:52 +0530

    Time: 6 ms (98.53%) | Memory: 45.6 MB (49.14%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -1,13 +1,21 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int mp=0;
+        int md=0,mp=0;
+        for(int i=0;i<difficulty.length;i++){
+            if(difficulty[i]>md)
+            md=difficulty[i];
+        }
+        int[] dp=new int[md+1];
+        for(int i=0;i<difficulty.length;i++){
+            dp[difficulty[i]]=Math.max(dp[difficulty[i]],profit[i]);
+        }
+        for(int i=1;i<=md;i++)
+        dp[i]=Math.max(dp[i],dp[i-1]);
         for(int i=0;i<worker.length;i++){
-            int p=0;
-            for(int j=0;j<difficulty.length;j++){
-                if(difficulty[j]<=worker[i] && profit[j]>p)
-                p=profit[j];
-            }
-            mp+=p;
+            if(worker[i]>md)
+            mp+=dp[md];
+            else
+            mp+=dp[worker[i]];
         }
         return mp;
     }
