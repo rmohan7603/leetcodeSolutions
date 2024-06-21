ae09eb30e95bd01f49a5dfd38bc143e6080c4ffe
R Mohan Krishna <141043010+rmohan7603@users.noreply.github.com>
2024-06-21 20:19:02 +0530

    Time: 3 ms (91.49%) | Memory: 47.4 MB (33.68%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -5,15 +5,20 @@ class Solution {
             if(grumpy[i]!=1)
             c+=customers[i];
         }
-        int d=0;
-        for(int i=0;i<=grumpy.length-minutes;i++){
-            int x=0;
-            for(int j=i;j<i+minutes;j++){
-                if(grumpy[j]!=0)
-                x+=customers[j];
-            }
-            d=Math.max(d,x);
+        int d=0,i;
+        for(i=0;i<minutes;i++){
+            if(grumpy[i]==1)
+            d+=customers[i];
         }
-        return c+d;
+        int s=d;
+        while(i<grumpy.length){
+            if(grumpy[i]==1)
+            d+=customers[i];
+            if(grumpy[i-minutes]==1)
+            d-=customers[i-minutes];
+            i+=1;
+            s=Math.max(d,s);
+        }
+        return c+s;
     }
 }
