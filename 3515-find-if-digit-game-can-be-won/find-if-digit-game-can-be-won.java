7899c76c0eb56d6350a92da3c413ba999774c414
R Mohan Krishna <141043010+rmohan7603@users.noreply.github.com>
2024-07-29 12:09:31 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (50.00%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean canAliceWin(int[] nums) {
-        int a=0,b=0,c=0;
+        int a=0,b=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]<10)
             a+=nums[i];
-            else if(nums[i]>9 && nums[i]<100)
-            b+=nums[i];
             else
-            c+=nums[i];
+            b+=nums[i];
         }
-        if(a>b+c || b>a+c)
+        if(a>b || b>a)
         return true;
         return false;
     }
