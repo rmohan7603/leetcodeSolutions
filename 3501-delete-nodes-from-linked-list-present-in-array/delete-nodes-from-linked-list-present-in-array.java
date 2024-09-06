6fa9e20e00c5dc704b7ab00aeef8e3b285a838a3
R Mohan Krishna <141043010+rmohan7603@users.noreply.github.com>
2024-09-06 09:33:41 +0530

    Time: 4 ms (99.55%) | Memory: 61.5 MB (99.85%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+/* class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
         Set<Integer> remSet=new HashSet<>();
         for(int i:nums)
@@ -18,6 +18,29 @@ class Solution {
         ListNode curNode=head;
         while(curNode.next!=null){
             if(remSet.contains(curNode.next.val))
+            curNode.next=curNode.next.next;
+            else
+            curNode=curNode.next;
+        }
+        return head;
+    }
+} */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        int maxVal=0;
+        for(int i:nums)
+        maxVal=Math.max(maxVal,i);
+        
+        int[] setArr=new int[maxVal+1];
+        for(int i:nums)
+        setArr[i]=1;
+        
+        while(head.val<=maxVal && setArr[head.val]==1)
+        head=head.next;
+        
+        ListNode curNode=head;
+        while(curNode.next!=null){
+            if(curNode.next.val<=maxVal && setArr[curNode.next.val]==1)
             curNode.next=curNode.next.next;
             else
             curNode=curNode.next;
