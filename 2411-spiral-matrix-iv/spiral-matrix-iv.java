653b56f4deed2e8111dd8146b9c065a8359648b3
R Mohan Krishna <141043010+rmohan7603@users.noreply.github.com>
2024-09-09 19:15:43 +0530

    Time: 7 ms (87.82%) | Memory: 56.6 MB (81.69%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -11,45 +11,39 @@
 class Solution {
     public int[][] spiralMatrix(int m, int n, ListNode head) {
         int[][] ansArr=new int[m][n];
+        for(int i=0;i<m;i++)
+        Arrays.fill(ansArr[i],-1);
         int top=0,bottom=m-1,left=0,right=n-1;
         while(top<=bottom && left<=right){
             for(int i=left;i<=right;i++){
+                ansArr[top][i]=head.val;
+                head=head.next;
                 if(head==null)
-                ansArr[top][i]=-1;
-                else{
-                    ansArr[top][i]=head.val;
-                    head=head.next;
-                }
+                return ansArr;
             }
             top+=1;
             for(int i=top;i<=bottom;i++){
+                ansArr[i][right]=head.val;
+                head=head.next;
                 if(head==null)
-                ansArr[i][right]=-1;
-                else{
-                    ansArr[i][right]=head.val;
-                    head=head.next;
-                }
+                return ansArr;
             }
             right-=1;
             if(top<=bottom){
                 for(int i=right;i>=left;i--){
+                    ansArr[bottom][i]=head.val;
+                    head=head.next;
                     if(head==null)
-                    ansArr[bottom][i]=-1;
-                    else{
-                        ansArr[bottom][i]=head.val;
-                        head=head.next;
-                    }
+                    return ansArr;
                 }
             }
             bottom-=1;
             if(left<=right){
                 for(int i=bottom;i>=top;i--){
+                    ansArr[i][left]=head.val;
+                    head=head.next;
                     if(head==null)
-                    ansArr[i][left]=-1;
-                    else{
-                        ansArr[i][left]=head.val;
-                        head=head.next;
-                    }
+                    return ansArr;
                 }
                 left+=1;
             }
